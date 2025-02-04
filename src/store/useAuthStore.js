943708f382a270971a676a59317ab4c49eeb88f3
Aditya Singh <yeezusgod01@gmail.com>
2025-02-05 00:28:04 +0530

    Added updated profile page

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -79,6 +79,22 @@ export const useAuthStore = create((set) => ({
             toast.error(error.response.data.message)
 
         }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set((state) => ({
+                authUser: { ...state.authUser, data: res.data.data },
+            }));
+            toast.success("Profile updated successfully");
+        } catch (error) {
+            toast.error(error?.response?.data?.message || "Failed to update profile");
+        } finally {
+            set({ isUpdatingProfile: false });
+        }
     }
+
 
 }))
