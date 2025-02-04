e98f8582c58fdef90360be27d77fa32db2d97d40
Aditya Singh <yeezusgod01@gmail.com>
2025-02-04 23:21:08 +0530

    Implemented login functionality with form handling

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -50,6 +50,23 @@ export const useAuthStore = create((set) => ({
 
     },
 
+    login: async (data) => {
+
+        set({ isLoggingIn: true })
+        try {
+
+            const response = await axiosInstance.post("/auth/login", data)
+            set({ authUser: response.data.data })
+            toast.success("Logged in successfully")
+
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isLoggingIn: false })
+        }
+
+    },
+
     logout: async () => {
         try {
 
