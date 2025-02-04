28e894c9061d20ba0873e9757ad0868db71d4d37
Aditya Singh <yeezusgod01@gmail.com>
2025-02-04 23:01:31 +0530

    Created the common navbar

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -48,6 +48,20 @@ export const useAuthStore = create((set) => ({
 
         }
 
+    },
+
+    logout: async () => {
+        try {
+
+            await axiosInstance.post("/auth/logout")
+            set({ authUser: null })
+            toast.success("Logged out successfully")
+
+        } catch (error) {
+
+            toast.error(error.response.data.message)
+
+        }
     }
 
 }))
